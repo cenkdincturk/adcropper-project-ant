a1ae934489f22c9bc578e6672946fda39b44b9e3
cenkdincturk <dincturkcenk@gmail.com>
2020-06-26 17:45:32 +0300

    singledropdown is changed. Project completed.

--- src/components/SingleDropdown.js
+++ src/components/SingleDropdown.js
@@ -1,36 +1,34 @@
 import React from "react";
 import { Menu, Dropdown, Button, message } from "antd";
-import "./SingleDropdown.css"
+import "./SingleDropdown.css";
+const card1 = require("../assets/card1.png");
+const card2 = require("../assets/card2.png");
+const card3 = require("../assets/card3.png");
+
 
 function SingleDropdown() {
 
-  function handleMenuClick(e) {
-    message.info("Image showed");
-    console.log("click", e);
-  }
+  const handleMenuClick = ({ key }) => {
+    message.info(`Click Option ${key}`);
+  };
 
   const menu = (
     <Menu onClick={handleMenuClick}>
-      <Menu.Item key="1">
-        Option 1
-      </Menu.Item>
-      <Menu.Item key="2">
-        Option 2
-      </Menu.Item>
-      <Menu.Item key="3">
-        Option 3
-      </Menu.Item>
+      <Menu.Item key="1">Option 1</Menu.Item>
+      <Menu.Item key="2">Option 2</Menu.Item>
+      <Menu.Item key="3">Option 3</Menu.Item>
     </Menu>
   );
 
   return (
     <div className="singledropdown">
-    <Dropdown overlay={menu}>
-      <Button className="button-color">
-        Dropdown
-      </Button>
-    </Dropdown>
-  </div>
+      <Dropdown overlay={menu}>
+        <Button className="button-color">
+          Dropdown
+        </Button>
+      </Dropdown>
+
+    </div>
   );
 }
 
